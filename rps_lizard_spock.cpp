043f4482fe9831d301bc8a867e7c8dcda515da38
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-29 15:36:40 -0800

    so far, so good

--- rps_lizard_spock.cpp
+++ rps_lizard_spock.cpp
@@ -1,3 +1,29 @@
+// C++ Standard Library input/output stream;
+#include <iostream>
+/* std::unordered_map is an unsorted associative container, which means that while its elements 
+consist of key-value pairs, they are not ordered based on the key as they would be in a std::map;
+the reason we use an unordered map is for simplicity and faster access speeds, the potential trade-offs
+are that the order is not guaranteed and may change over time, though that won't really matter for the
+purposes of this program; */
+#include <unordered_map>
+
+// Declare our rules to the game, it is simple text that will print to the terminal;
+void rules();
+
+
+
+// Define the rules function;
+void rules() {
+    printf("\nThis is Rock-Paper-Scissors-Lizard-Spock, from the hit TV show,\n"
+           "Big Bang Theory! It is similar to Rock-Paper-Scissors, but with\n"
+	   "two additional options: Lizard and Spock.\n\n"
+           "Nobody can explain the rules like Sheldon Cooper can, so I will\n" 
+	   "simply quote him: 'Scissors cuts paper, paper covers rock,\n" 
+	   "rock crushes lizard, lizard poisons Spock, Spock smashes scissors,\n" 
+	   "scissors decapitates lizard, lizard eats paper, paper disproves Spock,\n" 
+	   "Spock vaporizes rock, and as it always has, rock crushes scissors!'\n\n");
+}
+
 /* #include <stdio.h>
 // ctype.h contains tolower function, which converts all user-input text to
 // lower-case, to control for user error;
