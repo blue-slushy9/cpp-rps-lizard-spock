3748fa1ea0fb6b98d2869e5bd536954b785f5939
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-30 12:06:19 -0800

    rules function added successfully, program is running correctly thus far

--- rps_lizard_spock.cpp
+++ rps_lizard_spock.cpp
@@ -10,18 +10,23 @@ purposes of this program; */
 // Declare our rules to the game, it is simple text that will print to the terminal;
 void rules();
 
-
+// main function will contain all other functions in this program;
+int main() {
+    // Call the rules function;
+    rules();
+    return 0;
+}
 
 // Define the rules function;
 void rules() {
-    printf("\nThis is Rock-Paper-Scissors-Lizard-Spock, from the hit TV show,\n"
-           "Big Bang Theory! It is similar to Rock-Paper-Scissors, but with\n"
+    std::cout << ("\nThis is Rock-Paper-Scissors-Lizard-Spock, from the hit TV show,\n"
+       "Big Bang Theory! It is similar to Rock-Paper-Scissors, but with\n"
 	   "two additional options: Lizard and Spock.\n\n"
-           "Nobody can explain the rules like Sheldon Cooper can, so I will\n" 
+       "Nobody can explain the rules like Sheldon Cooper can, so I will\n" 
 	   "simply quote him: 'Scissors cuts paper, paper covers rock,\n" 
 	   "rock crushes lizard, lizard poisons Spock, Spock smashes scissors,\n" 
 	   "scissors decapitates lizard, lizard eats paper, paper disproves Spock,\n" 
-	   "Spock vaporizes rock, and as it always has, rock crushes scissors!'\n\n");
+	   "Spock vaporizes rock, and as it always has, rock crushes scissors!'\n\n") << std::endl;
 }
 
 /* #include <stdio.h>
